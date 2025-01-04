2b3b1b8b5073d9742b448a63fcf5566f43456da0
turik777 <52492191+turik777@users.noreply.github.com>
2025-01-05 00:07:54 +0300

    feat: complete vigenere cipher subtask

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let count = 0;
+    let encrypted = text.toUpperCase().split('').map((char, index) => {
+      if (alphabet.includes(char)) {
+        let textIndex = alphabet.indexOf(char);
+        let keyIndex = alphabet.indexOf(key.toUpperCase().charAt((index - count) % key.length));
+        let encryptedIndex = (textIndex + keyIndex) % alphabet.length;
+        return alphabet[encryptedIndex];
+      } else {
+        count++;
+        return char;
+      }
+    });
+    return this.direct ? encrypted.join('') : encrypted.reverse().join('');
+  }
+  decrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let count = 0;
+    let decrypted = text.toUpperCase().split('').map((char, index) => {
+      if (alphabet.includes(char)) {
+        let textIndex = alphabet.indexOf(char);
+        let keyIndex = alphabet.indexOf(key.toUpperCase().charAt((index - count) % key.length));
+        let decryptedIndex = (textIndex - keyIndex + alphabet.length) % alphabet.length;
+        return alphabet[decryptedIndex];
+      } else {
+        count++;
+        return char;
+      }
+    });
+    return this.direct ? decrypted.join('') : decrypted.reverse().join('');
   }
 }
 
