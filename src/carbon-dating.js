887a9da0c8b4e9335c0f21519d19c8d7757355a2
turik777 <52492191+turik777@users.noreply.github.com>
2025-01-04 04:09:07 +0300

    feat: complete carbon dating subtask

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+   return typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY
+    ? false
+    : Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
